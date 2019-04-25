4bbcc8d5e5541e9463d6cc54892699ee8cae5c66
Fady <ayoub77427@gmail.com>
2019-04-25 23:56:29 +0100

    ContactController created with index method that return contact view and a store method to store and send the mail to a local server but still not working and also validation with errors

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -7,40 +7,57 @@
 <div class="form-contact">
     <div class="container">
         <h4>Send me en Email</h4>
-        <form>  
+        <small class="form-text text-muted">My email is : ayoub77427@gmail.com</small>
+        @if(Session::has('flash_message'))
+            <div class="alert alert-success">
+                {{Session::get('flash_message')}}
+            </div>
+        @endif
+        <form method="POST" action="/contact">
+
+            {{ csrf_field() }}
+
             <div class="form-group">
             <label for="name">Name</label>
-            <input type="text" class="form-control" id="name">
+            <input type="text" class="form-control" id="name" name="name">
+                @if($errors->has('name'))
+                    <div class="invalid-feedback">
+                        {{$errors->first('name')}}
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
             <label for="email">Email address</label>
-            <input type="email" class="form-control" id="email">
-            <small class="form-text text-muted">My email is : ayoub77427@gmail.com</small>
+            <input type="email" class="form-control" id="email" name="email">
+                @if($errors->has('email'))
+                    <div class="invalid-feedback">
+                        {{$errors->first('email')}}
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
             <label for="subject">Subject</label>
-            <input type="text" class="form-control" id="subject">
+            <input type="text" class="form-control" id="subject" name="subject">
+                @if($errors->has('subject'))
+                    <div class="invalid-feedback">
+                        {{$errors->first('subject')}}
+                    </div>
+                @endif
             </div>
 
             <div class="form-group">
-            <label for="subject">Reason</label>
-            <select class="form-control" id="subject">
-                <option value="Just to say Hi">Just to say Hi</option>
-                <option value="Business">Business</option>
-                <option value="Software">Software</option>
-                <option value="Others">Others</option>
-            </select>
-            </div>
-
-            <div class="form-group">
-            <label for="subject">Subject</label>
-            <textarea class="form-control" id="subject"></textarea>
+            <label for="subject">Message</label>
+            <textarea class="form-control" id="subject" rows="5"name="message"></textarea>
+                @if($errors->has('message'))
+                    <div class="invalid-feedback">
+                        {{$errors->first('message')}}
+                    </div>
+                @endif
             </div>
 
             <button type="submit" class="btn btn-dark">Submit</button>
-
         </form>
     </div>
 </div>
